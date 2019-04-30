5da1cfe402e4cd275314840b6c79df62997f1de4
ka-zuumi <kazuumi@hawaii.edu>
2019-04-29 23:40:12 -1000

    Update 1.8
    Free at last, free at last.
    Thank god almighty, free at last!

--- script2.py
+++ script2.py
@@ -43,6 +43,21 @@ absolute_zero = -273.15
 # Current working directory
 cwd = os.getcwd() + "/"
 
+# Months
+months = [\
+        "Jan",\
+        "Feb",\
+        "Mar",\
+        "Apr",\
+        "May",\
+        "Jun",\
+        "Jul",\
+        "Aug",\
+        "Sep",\
+        "Oct",\
+        "Nov",\
+        "Dec"]
+
 ###############################################
 # Data Collection
 ###############################################
@@ -278,6 +293,25 @@ def getRadiance4(NTI,radiance12):
 def getFlyoverPeriod(latitude,longitude):
     return 1.1 * 24.0 / 2
 
+# Computes the Year,Month,Day,Hour,Min from
+# UNIX time
+
+def getUCT(seconds):
+    TotalSec = seconds
+    TotalMin = TotalSec//60
+    TotalHour = TotalMin//60
+    TotalDay = TotalHour//24
+    TotalMonth = TotalDay//30
+    TotalYear = TotalDay//365
+
+    Year = TotalYear + 1970
+    Month = TotalMonth % 12
+    Day = TotalDay % 30
+    Hour = TotalHour % 24
+    Min = TotalMin % 60
+
+    return Year,Month,Day,Hour,Min
+
 ###############################################
 # Experiment 3
 ###############################################
@@ -297,16 +331,35 @@ time_per_frame = \
         getFlyoverPeriod(0.0,0.0) / 2
 frames_per_second = 4
 
+lava_fraction = 0.50
+
 minSTARTtime = getTimeCooled(500.0,27.0,0.90)
 #minSTARTtime = getTimeCooled(1400.0,27.0,0.90)
 
-STARTtime = 0.00
+#STARTtime = 0.00
+STARTtime = 16.00
 
 STARTtime = STARTtime + minSTARTtime
+
+latitude1 = 19.38
+longitude1 = -155.1
+radius1  = 0.09
+
+lat1 = (latitude1 - (center_lat - dcenter_lat)) / \
+        (dlat*Nlat)
+long1 = (longitude1 - (center_long - dcenter_long)) / \
+        (dlong*Nlong)
+rad1 = radius1 / (0.5 * (dlat + dlong) \
+        * 0.5 * (Nlat + Nlong))
 
 gif_start_time = UNIXtimes[0]
 gif_end_time = gif_start_time + \
         100 * 60*60*getFlyoverPeriod(0.0,0.0)
+
+YearI,MonthI,DayI,HourI,MinI = getUCT(\
+        int(gif_start_time))
+YearE,MonthE,DayE,HourE,MinE = getUCT(\
+        int(gif_end_time))
 
 os.system(("rm -r "+cwd+"png/"))
 os.system(("mkdir "+cwd+"png/"))
@@ -331,7 +384,10 @@ def makeImages(local_observable,\
     print("")
 
     min_radiance = 0.0
-    max_radiance = 15.0
+    max_radiance = 18.0
+
+#   Year,Month,Day,Hour,Min = getUCT(\
+#           UNIXtimeData[Nunique])
     
     # Output our data onto some file
     exp003file = open(cwd+\
@@ -379,24 +435,38 @@ def makeImages(local_observable,\
     
     exp003file.close()
  
-    # Output our data onto some file
-    exp003file = open(cwd+'exp003.dat',"w")
-
-    for j in range(Nunique):
-        UNIXtime = UNIXtimeData[j+1]
-        radiance = maxRadianceData[j+1]
-        exp003file.write(\
-                f'{UNIXtime} ' + \
-                f'{radiance}\n')
-    
-    exp003file.close()
-    
 ###############################################
 # Data Visualization
 ###############################################
      
     for n in range(total_indexes):
+
         nframe = start_index + n + 1
+ 
+        # Output our data onto some file
+        exp003file = open(cwd+'exp003.dat',"a")
+
+        Year,Month,Day,Hour,Min = getUCT(\
+                int(gif_start_time + \
+                (nframe-1) * \
+                60 * 60 * time_per_frame))
+    
+        if (n == 0):
+            UNIXtime = UNIXtimeData[Nunique]
+            radiance = maxRadianceData[Nunique]
+            exp003file.write(\
+                    f'{UNIXtime} ' + \
+                    f'{radiance} ' + \
+                    '0 0\n')
+        else:
+            UNIXtime = UNIXtimeData[Nunique]+n
+            radiance = -1
+            exp003file.write(\
+                    f'{UNIXtime} ' + \
+                    f'{radiance} ' + \
+                    '0 0\n')
+        
+        exp003file.close()
 
         # Make the gnuplot script that will
         # visualize our data
@@ -410,7 +480,7 @@ def makeImages(local_observable,\
                 'png/{:04d}.png"\n').format(nframe))
         gw(f'set multiplot\n')
         gw('set title ' + \
-            '"Radiances over Kileaua" ' + \
+            '"Radiances over Kileauea" ' + \
             'font ",36" offset 0,3\n')
         gw('set size 0.5, 1.0\n')
         gw('set origin 0.0, 0.0\n')
@@ -441,8 +511,8 @@ def makeImages(local_observable,\
                 '0.2 "yellow", ' + \
                 '1 "red"' + \
                 ')\n')
-        gw('set xlabel "Longitude" font ",24"\n')
-        gw('set ylabel "Latitude" font ",24"\n')
+        gw('set ylabel "Longitude" font ",24"\n')
+        gw('set xlabel "Latitude" font ",24"\n')
         gw('set cblabel "Radiance" font ",24"\n')
     #   gw('set format x ""\n')
     #   gw('set grid xtics lt 0 lw 2\n')
@@ -450,6 +520,7 @@ def makeImages(local_observable,\
         gw('unset ytics\n')
         gw('set ytics (' + \
                 f'"{center_long-dcenter_long}" 0, ' + \
+                f'"{center_long}" {(Nlong-1)/2}, ' + \
                 f'"{center_long+dcenter_long}" {Nlong-1}' + \
                 ')\n')
         gw('unset xtics\n')
@@ -460,6 +531,10 @@ def makeImages(local_observable,\
     
 #       gw('plot "'+cwd+'exp003heatmap.dat" ' + \
 #               'matrix u 1:2:3 w image\n')
+
+        gw('set object 1 circle at graph ' + \
+                f'{lat1},{long1},1 size graph ' + \
+                f'{rad1} fc rgb "black" front\n')
         gw('plot "'+cwd+'exp003heatmap.dat" ' + \
                 'u 1:2:3 w image\n')
     
@@ -473,14 +548,52 @@ def makeImages(local_observable,\
         gw(f'xmax = {gif_end_time}\n')
         gw('set xrange [xmin:xmax]\n')
         gw('set yrange [cbmin:cbmax]\n')
-        gw('set xlabel "UNIXtime" font ",24"\n')
-        gw('set ylabel "Radiance" font ",24"\n')
+        gw('set xlabel "Time" font ",18"\n')
+        gw('set ylabel "Max Radiance" font ",18"\n')
         gw('unset ytics\n')
         gw('set ytics\n')
         gw('unset xtics\n')
-        gw('set xtics\n')
+        gw('set xtics (' + \
+                ('"{:02d} {:3} {:04d}"').format(\
+                DayI,months[MonthI],YearI) + \
+                f' {gif_start_time}, ' + \
+                ('"{:02d} {:3} {:04d}"').format(\
+                DayE,months[MonthE],YearE) + \
+                f' {gif_end_time})\n')
+        gw('unset object 1\n')
+        gw('set key\n')
+        gw('set label 2 ' + \
+                ('"{:02d} {:3} {:04d}').format(\
+                Day+1,months[Month],Year) + \
+                '" at screen 0.9,0.95 ' + \
+                'font ",18"\n')
+        gw('set label 3 ' + \
+                '"Lava Cooling Start Time: ' + \
+                ('{:6.2f} h" ').format(\
+                STARTtime) + \
+                'at screen 0.85, 0.925 ' + \
+                'font ",14"\n')
+        gw('set label 4 ' + \
+                '"Percent Lava Coverage: ' + \
+                f'{100*lava_fraction} \%" ' + \
+                'at screen 0.85, 0.900 ' + \
+                'font ",14"\n')
         gw('plot "'+cwd+'exp003.dat" ' + \
-                'u 1:2 w l')
+                'u 1:($2>0?$2:1/0) w l ' + \
+                'lw 3 lc "purple" t "",\\\n')
+        gw('     "'+cwd+'exp003.dat" ' + \
+                'u 1:($2>0?$2:1/0) w p ' + \
+                'pt 7 ps 2 lc "purple" t "",\\\n')
+        gw('     "'+cwd+'exp003.dat" ' + \
+                'u 1:($3==1?$2:1/0) ' + \
+                'w p lc "green" pt 7 ps 2 t "Anomalous",\\\n')
+        gw('     "'+cwd+'exp003.dat" ' + \
+                'u 1:($4==1?$2:1/0) ' + \
+                'w p lc "red" pt 7 ps 2 t "Suspicious",\\\n')
+        gw('     "'+cwd+'exp003.dat" ' + \
+                'u 1:($3==1&&$4==1?$2:1/0) ' + \
+                'w p lc "orange-red" pt 7 ps 2 t "Anomalous and Suspicious"')
+
         
         gnuplotfile.close()
         
@@ -500,6 +613,7 @@ deltaTime_max = \
 # Records how many unique flyovers
 # occured
 Nunique = 0
+Nframes = 0
 Nframes_total = 0
 
 # Record the maximum radiance for
@@ -525,15 +639,14 @@ local_4radiances = [\
         for i in range(Nlong)]\
         for j in range(Nlat)]
 
-maxRadiance = 0
+os.system('rm ' + cwd + 'exp003.dat')
+
+maxRadiance = -1
 Nhotspot = 0
 missingData = [1]
 #anomalousData = [1]
 anomalousData = []
 suspiciousData = [1]
-
-latitude1 = -155.6
-longitude1 = 19.2
 
 for i in range(N-1):
 
@@ -582,16 +695,16 @@ for i in range(N-1):
     if (nlong_max >= Nlong) : \
             nlong_max = Nlong - 1
 
-    print(Nlat)
-    print(Nlat*dlat)
-    print(nlat_min)
-    print(nlat_max)
-    print(Nlong)
-    print(Nlong*dlong)
-    print(nlong_min)
-    print(nlong_max)
-    print(currentRadiance)
-    print("")
+#   print(Nlat)
+#   print(Nlat*dlat)
+#   print(nlat_min)
+#   print(nlat_max)
+#   print(Nlong)
+#   print(Nlong*dlong)
+#   print(nlong_min)
+#   print(nlong_max)
+#   print(currentRadiance)
+#   print("")
     
     for j in range(nlat_max-nlat_min+1):
         for k in range(nlong_max-nlong_min+1):
@@ -600,8 +713,11 @@ for i in range(N-1):
             local_4radiances[nlat][nlong] =\
                     currentRadiance
 
-    if (currentRadiance\
-        > maxRadiance):
+    if ((currentRadiance\
+        > maxRadiance) and \
+        ((latitude1-latitudes[i])**2 + \
+        (longitude1-longitudes[i])**2 \
+        < radius1**2)):
         maxRadiance =\
             currentRadiance
         otherRadiance =\
@@ -621,6 +737,17 @@ for i in range(N-1):
             Nhotspot + 1
         continue
 
+    if (Nframes == 0):
+        UNIXtimeData.append(
+                UNIXtimes[i])
+
+    Nframes = Nframes + \
+            (deltaTime/\
+            time_per_frame)
+
+    if (maxRadiance < 0):
+        continue
+
     # Otherwise, calculate the
     # maximum radiance and
     # record this on our time
@@ -629,21 +756,17 @@ for i in range(N-1):
         maxRadiance)
     NhotspotData.append(
         Nhotspot + 1)
-    UNIXtimeData.append(
-        UNIXtimes[i])
 
     # This was one unique flyover
     Nunique = Nunique + 1
 
-    Nframes = np.floor(deltaTime/\
-            time_per_frame)
-
     makeImages(local_4radiances,\
             int(Nframes_total),\
-            int(Nframes))
+            int(np.floor(Nframes)))
 
     Nframes_total = \
-            Nframes_total + Nframes
+            Nframes_total + \
+            np.floor(Nframes)
 
     if (i < N-1): \
     background_4radiance = \
@@ -661,12 +784,12 @@ for i in range(N-1):
     deltaRadiance_max = \
         getSpectralRadiance2(\
         getSurfaceTemp(STARTtime),\
-        temps[i],0.90,\
+        temps[i],lava_fraction,\
         wavelengths[0])\
       - getSpectralRadiance2(\
         getSurfaceTemp(STARTtime\
           + deltaTime),\
-        temps[i],0.90,\
+        temps[i],lava_fraction,\
         wavelengths[0])
 
     # And if we have dropped this
@@ -714,10 +837,37 @@ for i in range(N-1):
         missingData.append(0)
         suspiciousData.append(0)
 
-    # Reset the values
-    maxRadiance = 0
-    Nhotspot = 0
+    if (Nunique > 1):
+        os.system("sed -i '" + \
+                f's/{UNIXtimeData[Nunique-1]}' + \
+                f' {maxRadianceData[Nunique-1]}' + \
+                ' 0 0' + \
+                f'/{UNIXtimeData[Nunique-1]}' + \
+                f' {maxRadianceData[Nunique-1]}' + \
+                f' {anomalousData[Nunique-1]}' + \
+                f" {suspiciousData[Nunique-1]}/' " + \
+                cwd + 'exp003.dat')
 
+        print("sed -i '" + \
+                f's/{UNIXtimeData[Nunique-1]}' + \
+                f' {maxRadianceData[Nunique-1]}' + \
+                ' 0 0/' + \
+                f's/{UNIXtimeData[Nunique-1]}' + \
+                f' {maxRadianceData[Nunique-1]}' + \
+                f' {anomalousData[Nunique-1]}' + \
+                f" {suspiciousData[Nunique-1]}/' " + \
+                cwd + 'exp003.dat')
+
+    # Reset the values
+    maxRadiance = -1
+    Nhotspot = 0
+    Nframes = 0
+
+makeImages(local_4radiances,\
+        int(Nframes_total),\
+        int(Nframes))
+
+os.system('rm '+cwd+'exp003.mp4')
 os.system(f'ffmpeg -r {frames_per_second} ' + \
         '-f image2 -s 1920x1080 ' + \
         '-i ' + cwd +'png/%04d.png ' + \
